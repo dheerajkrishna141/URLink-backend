c9954febc3e6349d0ba9893e9e04c17a979fbea4
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2024-06-27 16:00:01 -0400

    Moved the ModelMapper bean to General Config

--- src/main/java/com/example/urlshortener/UrlshortenerApplication.java
+++ src/main/java/com/example/urlshortener/UrlshortenerApplication.java
@@ -1,10 +1,7 @@
 package com.example.urlshortener;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
@@ -13,9 +10,6 @@ public class UrlshortenerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UrlshortenerApplication.class, args);
 	}
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+	
 	
 }
